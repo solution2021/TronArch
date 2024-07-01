d5aaaebd71f192b5d52d35b8445123bafddae3aa
ZK SDT <93309984+solution2021@users.noreply.github.com>
2024-07-01 16:37:32 +0900

    Add files via upload

---
+++ ZwMenu/ZYXCAD_2022_ZRXSDK/arxport/IAdHostWindow.h
@@ -0,0 +1,12 @@
+﻿
+#ifndef  __IADHOSTWINDOW_H__
+#define __IADHOSTWINDOW_H__
+
+#include "adhostableui.h"
+#include "../inc/zIAdHostWindow.h"
+
+#ifndef IAdHostWindow
+#define   IAdHostWindow  IZdHostWindow
+#endif	//#ifndef IAdHostWindow
+
+#endif //__IADHOSTWINDOW_H__
