1676e73ede86c2648ef8f209946f4f8ed7a26c80
ZK SDT <93309984+solution2021@users.noreply.github.com>
2024-07-01 16:30:48 +0900

    Add files via upload

---
+++ ZwMenu/ZYXCAD_2022_ZRXSDK/arxport/asiclass.h
@@ -0,0 +1,10 @@
+﻿
+#ifndef __ASICLASS_H__
+#define __ASICLASS_H__
+
+#include <asisdata.h>
+#include <avoidtry.h> 
+
+#include "../inc/zasiclass.h"
+
+#endif //#ifndef __ASICLASS_H__
---
+++ ZwMenu/ZYXCAD_2022_ZRXSDK/arxport/asisys.h
@@ -0,0 +1,9 @@
+﻿
+#ifndef __ASISYS_H__
+#define __ASISYS_H__
+
+#include <tchar.h>
+
+#include "../inc/zasisys.h"
+
+#endif //#ifndef __ASISYS_H__
