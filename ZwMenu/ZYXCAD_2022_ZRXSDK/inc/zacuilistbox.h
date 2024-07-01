614865e1373432372fb69113c13dffcf0539f9ad
ZK SDT <93309984+solution2021@users.noreply.github.com>
2024-07-01 16:46:11 +0900

    Add files via upload

---
+++ ZwMenu/ZYXCAD_2022_ZRXSDK/inc/zacuilistbox.h
@@ -0,0 +1,16 @@
+﻿
+
+#ifndef _zcuiListBox_h
+#define _zcuiListBox_h
+#pragma pack (push, 8)
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif 
+
+#define CZcUiListBox CZdUiListBox
+
+#pragma pack (pop)
+#endif
+
+
---
+++ ZwMenu/ZYXCAD_2022_ZRXSDK/inc/zacuilistctrl.h
@@ -0,0 +1,16 @@
+﻿
+
+#ifndef _zcuiListCtrl_h
+#define _zcuiListCtrl_h
+#pragma pack (push, 8)
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif 
+
+#define  CZcUiListCtrl CZdUiListCtrl
+
+#pragma pack (pop)
+#endif
+
+
